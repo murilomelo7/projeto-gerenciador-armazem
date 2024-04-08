2ae6ca0b79f2cd5d9715900548145abcc165869e
Murilo Melo <megamurilo2@gmail.com>
2024-04-07 22:57:49 -0300

    feature
    adicionado empresa_id em todas as tabelas para que possamos ter diversos clientes

--- backend/prisma/migrations/20240401133453_adicionado/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `descricao` to the `Categoria` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Categoria" ADD COLUMN     "descricao" TEXT NOT NULL;
