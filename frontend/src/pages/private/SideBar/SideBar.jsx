cc2fd2a85a5891c707a8f2e526c5ef8062150a4e
Fernandomascarelli <fernandobiguetti123@gmail.com>
2024-04-27 18:39:16 -0300

    demonio
    demonio

---
+++ frontend/src/pages/private/SideBar/SideBar.jsx
@@ -0,0 +1,146 @@
+import './sideBarStyle.css';
+import './app.js'
+
+const Teste = () => {
+  return (
+    <div>
+      {/* <nav>
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet' />
+      <div className="sidebar-top">
+        <span className="shrink-btn">
+          <i className='bx bx-chevron-left'></i>
+        </span>
+        <img src="/src/assets/logo.png" className="logo" alt="" />
+        <h3 className="hide">NandeRellis</h3>
+      </div>
+
+      <div className="search">
+        <i className='bx bx-search'></i>
+        <input type="text" className="hide" placeholder="Buscar ..." />
+      </div>
+
+      <div className="sidebar-links">
+        <ul>
+          <div className="active-tab"></div>
+          <li className="tooltip-element" data-tooltip="0">
+            <a href="#" className="active" data-active="0">
+              <div className="icon">
+                <i className='bx bx-tachometer'></i>
+                <i className='bx bxs-tachometer'></i>
+              </div>
+              <span className="link hide">Dashboards</span>
+            </a>
+          </li>
+          <li className="tooltip-element" data-tooltip="1">
+            <a href="#" data-active="1">
+              <div className="icon">
+                <i className='bx bx-folder'></i>
+                <i className='bx bxs-folder'></i>
+              </div>
+              <span className="link hide">Empresas</span>
+            </a>
+          </li>
+          <li className="tooltip-element" data-tooltip="2">
+            <a href="#" data-active="2">
+              <div className="icon">
+                <i className='bx bx-message-square-detail'></i>
+                <i className='bx bxs-message-square-detail'></i>
+              </div>
+              <span className="link hide">Notificações</span>
+            </a>
+          </li>
+          <li className="tooltip-element" data-tooltip="3">
+            <a href="#" data-active="3">
+              <div className="icon">
+                <i className='bx bx-bar-chart-square'></i>
+                <i className='bx bxs-bar-chart-square'></i>
+              </div>
+              <span className="link hide">Analytics</span>
+            </a>
+          </li>
+          <div className="tooltip">
+            <span className="show">Dashboard</span>
+            <span>Empresas</span>
+            <span>Notificações</span>
+            <span>Analytics</span>
+          </div>
+        </ul>
+
+        <h4 className="hide">Atalhos</h4>
+
+        <ul>
+          <li className="tooltip-element" data-tooltip="0">
+            <a href="#" data-active="4">
+              <div className="icon">
+                <i className='bx bx-notepad'></i>
+                <i className='bx bxs-notepad'></i>
+              </div>
+              <span className="link hide">Tarefas</span>
+            </a>
+          </li>
+          <li className="tooltip-element" data-tooltip="1">
+            <a href="#" data-active="5">
+              <div className="icon">
+                <i className='bx bx-help-circle'></i>
+                <i className='bx bxs-help-circle'></i>
+              </div>
+              <span className="link hide">Ajuda</span>
+            </a>
+          </li>
+          <li className="tooltip-element" data-tooltip="2">
+            <a href="#" data-active="6">
+              <div className="icon">
+                <i className='bx bx-cog'></i>
+                <i className='bx bxs-cog'></i>
+              </div>
+              <span className="link hide">Configurações</span>
+            </a>
+          </li>
+          <div className="tooltip">
+            <span className="show">Tarefas</span>
+            <span>Ajuda</span>
+            <span>Configurações</span>
+          </div>
+        </ul>
+      </div>
+
+      <div className="sidebar-footer">
+        <a href="#" className="account tooltip-element" data-tooltip="0">
+          <i className='bx bx-user'></i>
+        </a>
+        <div className="admin-user tooltip-element" data-tooltip="1">
+          <div className="admin-profile hide">
+            <img src="./img/face-1.png" alt="" />
+            <div className="admin-info">
+              <h3>Admin</h3>
+              <h5>Admin</h5>
+            </div>
+          </div>
+          <a href="#" className="log-out">
+            <i className='bx bx-log-out'></i>
+          </a>
+        </div>
+        <div className="tooltip">
+          <span className="show">Admin</span>
+          <span>Sair</span>
+        </div>
+      </div>
+
+      <main>
+    <h1>Dashboards Gerais</h1>
+    <p className="text">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur animi voluptatibus cum maxime distinctio
+      iste quod deleniti eius, autem voluptates cumque suscipit iure quasi eligendi ullam. Sapiente eligendi porro
+      reprehenderit corrupti error facilis quo, fugiat fugit? Maiores aliquam ad, molestiae iste nihil, commodi
+      doloremque tempore excepturi aut id ducimus unde?
+    </p>
+    <p className="copyright">
+      &copy; 2024 - <span>NadeRellis</span> Todos os direitos reservados.
+    </p>
+  </main>
+      </nav> */}
+    </div>
+  );
+};
+
+export default Teste;
--- frontend/src/pages/private/private.routes.jsx
+++ frontend/src/pages/private/private.routes.jsx
@@ -3,17 +3,17 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  RouterProvider,
-  Navigate,
 } from "react-router-dom";
 
 import Dashboard from "./Dashboard/Dashboard";
+// import Teste from "./SideBar/SideBar";
 
 const PrivateRoutes = () => {
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/berimbola" element={<Teste/>}/> */}
         </Routes>
     </BrowserRouter>
   );
